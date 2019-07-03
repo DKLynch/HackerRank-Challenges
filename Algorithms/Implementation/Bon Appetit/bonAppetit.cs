648e520fab14824a3620e63c22ff18dbc4ac5ed6
David Lynch <DLynchContact@gmail.com>
2019-07-04 00:21:01 +0100

    Indentation Fix Test

--- Algorithms/Implementation/Bon Appetit/bonAppetit.cs
+++ Algorithms/Implementation/Bon Appetit/bonAppetit.cs
@@ -1,15 +1,15 @@
 //Problem URL: https://www.hackerrank.com/challenges/bon-appetit/problem
 
 static void bonAppetit(List<int> bill, int k, int b) {
-	var otherItems = bill.Where((x, y) => y != k);
+    var otherItems = bill.Where((x, y) => y != k);
 
-	int total = 0;
-	foreach(int price in otherItems){
-		total += price;
-	}
+    int total = 0;
+    foreach(int price in otherItems){
+        total += price;
+    }
 
-	if((total / 2) == b)
-		Console.Write("Bon Appetit");
-	else
-		Console.Write(b - (total / 2));
+    if((total / 2) == b)
+        Console.Write("Bon Appetit");
+    else
+        Console.Write(b - (total / 2));
 }
